9060c927208f6354c3cf298c91fe8558996ad586
dannyjb164 <33274962+dannyjb164@users.noreply.github.com>
2022-04-06 20:36:59 +1200

    Created potions class

---
+++ monster-fighter/src/Potions.java
@@ -0,0 +1,21 @@
+public class Potions implements Items{
+
+	@Override
+	public void changeStrength() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeHealth() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeDefense() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
