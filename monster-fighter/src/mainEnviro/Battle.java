17c1ecbe19ed6df5056c5a531e33100448b39447
dannyjb164 <dannyjb164@gmail.com>
2022-04-07 12:23:05 +1200

    Created battle class

---
+++ monster-fighter/src/mainEnviro/Battle.java
@@ -0,0 +1,21 @@
+package mainEnviro;
+import monster.*;
+import java.lang.Math;
+import java.util.Random;
+
+public class Battle {
+	
+	private Monster enemy;
+	private Monster user;
+	
+	public void fight() {
+		while (enemy.getCurrHealth() > 0 && user.getCurrHealth() > 0) {
+			int enemyChoice = enemyChoice();
+		}
+	}
+	
+	public static int enemyChoice() {
+		double rand = Math.random();
+		return (int)(rand * 3);
+	}
+}
