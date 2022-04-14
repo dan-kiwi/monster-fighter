e383d4fb52171983c322d22dc3168725c124c299
dannyjb164 <dannyjb164@gmail.com>
2022-04-14 14:32:55 +1200

    Creating battle class

--- monster-fighter/src/mainEnviro/Battle.java
+++ monster-fighter/src/mainEnviro/Battle.java
@@ -1,6 +1,7 @@
 package mainEnviro;
 
-import monster.*;
+import monster.Monster;
+import monster.Dragon;
 import java.lang.Math;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,21 +10,33 @@ import java.util.Random;
 
 public class Battle {
 	
-	//private static List<Monster> enemies = List.of(new Monster Dragon);
+	private static ArrayList<Monster> enemies = new ArrayList<Monster>() {{
+		add(new Dragon());
+	}};
 	
 	private ArrayList<Monster> potentialBattles = new ArrayList<Monster>();
+	private ArrayList<Monster> userMonsterList;
 	private Monster currEnemy;
 	private Monster currUser;
 	private Random rand = new Random();
 	private String test;
 	
-	public Battle(ArrayList<Monster> enemyList) {
+	public Battle() {
+		this.userMonsterList = userMonsterList;
 		int randNumBattles = rand.nextInt(3, 5);
 		for (int i = 0; i < randNumBattles; i++) { // creates random number of battles between 3 & 5
-			int randIndexEnemy = rand.nextInt(enemyList.size()); 
-			BattleEvent potentialBattle = new BattleEvent(enemyList.get(randIndexEnemy));
-			//potentialBattles.add(potentialBattle);
+			int randIndexEnemy = rand.nextInt(enemies.size()); 
+			potentialBattles.add(enemies.get(randIndexEnemy));
 		}
+	}
+	
+	public void selectEnemies() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Battle test = new Battle();
+		System.out.println(enemies);
 	}
 	
 }
