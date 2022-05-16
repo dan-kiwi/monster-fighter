92e81338fb76fe5dd61a87e7b85f5cedd015edd3
dannyjb164 <33274962+dannyjb164@users.noreply.github.com>
2022-05-16 17:57:38 +1200

    Updated javadoc for MonsterTest

--- monster-fighter/src/test/MonstersTest.java
+++ monster-fighter/src/test/MonstersTest.java
@@ -13,30 +13,45 @@ class MonstersTest {
 	private Monster monster;
 	private Monster monster2;
 	
+	/**
+	 * Resets each monster before each test
+	 * Uses two objects of the same class to compare the effect of level up compared to another
+	 */
 	@BeforeEach
 	public void testEachMonster() {
 		monster = new Unicorn();
 		monster2 = new Unicorn();
 	}
 	
+	/**
+	 * Tests the change of Health on the monsters
+	 * Ensures that it does not go above max health or below zero
+	 */
 	@Test
 	public void checkHealth() {
 		assertEquals(75, monster.getCurrHealth());
 		monster.setCurrHealth(50);
 		assertEquals(50, monster.getCurrHealth());
-		monster.setCurrHealth(100);
-		assertEquals(75, monster.getCurrHealth());
-		monster.changeCurrHealth(50);
-		assertEquals(75, monster.getCurrHealth());
+		monster.setCurrHealth(monster.getMaxHealth() + 10); //checks setCurrHealth
+		assertEquals(monster.getMaxHealth(), monster.getCurrHealth());
+		monster.changeCurrHealth(50); //checks changeCurrHealth
+		assertEquals(monster.getMaxHealth(), monster.getCurrHealth());
 		monster.changeCurrHealth(-50);
-		assertEquals(25, monster.getCurrHealth());
-		monster.changeCurrHealth(-50);
+		assertEquals(monster.getMaxHealth() - 50, monster.getCurrHealth());
+		monster.changeCurrHealth(-50000);
+		assertEquals(0, monster.getCurrHealth());
+		monster.setCurrHealth(-50000);
 		assertEquals(0, monster.getCurrHealth());
 	}
 	
+	/**
+	 * Checks monsters stats are reset
+	 * Ensures that not everything gets reset
+	 */
 	@Test
 	public void resetMonsterStats() {
 		monster.addDailyBattlesWon(1);
+		monster.changeCurrHealth(-50);
 		assertEquals(1, monster.getDailyBattlesWon());
 		assertEquals(1, monster.getTotalBattlesWon());
 		monster.resetMonsterStats();
@@ -45,6 +60,9 @@ class MonstersTest {
 		assertEquals(75, monster.getCurrHealth());
 	}
 	
+	/**
+	 * Checks that when leveling up, all attributes are greater
+	 */
 	@Test
 	public void checkLevelUp() {
 		monster.levelUp();
