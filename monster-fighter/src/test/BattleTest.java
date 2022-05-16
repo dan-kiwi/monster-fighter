ef37cdc6fb39a2f521c2fde8478127194c704ce5
dannyjb164 <dannyjb164@gmail.com>
2022-05-16 13:53:01 +1200

    Added battle JUnit test

---
+++ monster-fighter/src/test/BattleTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import mainenviro.Battle;
+import monster.Gnome;
+import monster.Monster;
+import monster.Unicorn;
+
+class BattleTest {
+	
+	Monster user;
+	Monster enemy;
+	Battle battle = new Battle(null); //need to fix
+
+	@Test
+	void selectingUser() {
+		fail("Not yet implemented");
+	}
+
+}
