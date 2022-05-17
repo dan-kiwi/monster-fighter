681019f0e5ba31b6b31f31fe34af63de11dc8fa5
dannyjb164 <33274962+dannyjb164@users.noreply.github.com>
2022-05-17 15:16:45 +1200

    JUnit test for game enviro

--- monster-fighter/src/test/GameEnviroTest.java
+++ monster-fighter/src/test/GameEnviroTest.java
@@ -2,13 +2,26 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import mainenviro.GameEnviro;
 
 class GameEnviroTest {
+	
+	GameEnviro game = new GameEnviro();
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testDifficulty() {
+		game.starterSetDifficulty("Hard");
+		assertEquals(game.getUserGoldAmount(), 150);
+		assertEquals(game.getGoldDifficulty(), 0.85);
+		assertEquals(game.getMonsterDifficulty(), 1.15);
+		game.starterSetDifficulty("Easy");
+		assertEquals(game.getUserGoldAmount(), 300);
+		assertEquals(game.getGoldDifficulty(), 1.5);
+		assertEquals(game.getMonsterDifficulty(), 0.85);
 	}
 
 }
