806fc2405e5e40d8c79275f31b61ae7af57d03ee
dannyjb164 <dannyjb164@gmail.com>
2022-05-22 23:31:53 +1200

    Updated fight damage screen

--- monster-fighter/src/gui/ViewFightDamageScreen.java
+++ monster-fighter/src/gui/ViewFightDamageScreen.java
@@ -8,6 +8,7 @@ import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
 
 /**
  * The Class ViewFightDamageScreen.
@@ -54,32 +55,36 @@ public class ViewFightDamageScreen {
 	private void initialize() {
 		frmViewFightDamage = new JFrame();
 		frmViewFightDamage.setTitle("Damage Done");
-		frmViewFightDamage.setBounds(100, 100, 450, 300);
+		frmViewFightDamage.setBounds(100, 100, 560, 575);
 		frmViewFightDamage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmViewFightDamage.getContentPane().setLayout(null);
 		
 		JLabel lblFightDamageUserAction = new JLabel(gameEnviro.getBattle().getCurrUser().getMonsterName()
 				+ " used " + userChoice + ".");
+		lblFightDamageUserAction.setHorizontalAlignment(SwingConstants.CENTER);
 		lblFightDamageUserAction.setFont(new Font("Verdana", Font.BOLD, 13));
-		lblFightDamageUserAction.setBounds(10, 11, 414, 24);
+		lblFightDamageUserAction.setBounds(50, 130, 450, 24);
 		frmViewFightDamage.getContentPane().add(lblFightDamageUserAction);
 		
 		JLabel lblFightDamageUserDamage = new JLabel(gameEnviro.getBattle().getCurrUser().getMonsterName()
 				+ " did " + userDamageDone + " Damage.");
+		lblFightDamageUserDamage.setHorizontalAlignment(SwingConstants.CENTER);
 		lblFightDamageUserDamage.setFont(new Font("Verdana", Font.BOLD, 13));
-		lblFightDamageUserDamage.setBounds(10, 35, 414, 24);
+		lblFightDamageUserDamage.setBounds(50, 165, 450, 24);
 		frmViewFightDamage.getContentPane().add(lblFightDamageUserDamage);
 		
 		JLabel lblFightDamageEnemyAction = new JLabel(gameEnviro.getBattle().getCurrEnemy().getMonsterName()
 				+ " used " + enemyChoice + ".");
+		lblFightDamageEnemyAction.setHorizontalAlignment(SwingConstants.CENTER);
 		lblFightDamageEnemyAction.setFont(new Font("Verdana", Font.BOLD, 13));
-		lblFightDamageEnemyAction.setBounds(10, 87, 414, 24);
+		lblFightDamageEnemyAction.setBounds(50, 285, 450, 25);
 		frmViewFightDamage.getContentPane().add(lblFightDamageEnemyAction);
 		
 		JLabel lblFightDamageEnergyDamage = new JLabel(gameEnviro.getBattle().getCurrEnemy().getMonsterName()
 				+ " did " + enemyDamageDone + " Damage.");
+		lblFightDamageEnergyDamage.setHorizontalAlignment(SwingConstants.CENTER);
 		lblFightDamageEnergyDamage.setFont(new Font("Verdana", Font.BOLD, 13));
-		lblFightDamageEnergyDamage.setBounds(10, 110, 414, 24);
+		lblFightDamageEnergyDamage.setBounds(50, 320, 450, 25);
 		frmViewFightDamage.getContentPane().add(lblFightDamageEnergyDamage);
 		
 		JButton btnFightDamage = new JButton("Continue");
@@ -110,8 +115,26 @@ public class ViewFightDamageScreen {
 			}
 		});
 		btnFightDamage.setFont(new Font("Verdana", Font.BOLD, 15));
-		btnFightDamage.setBounds(134, 174, 170, 51);
+		btnFightDamage.setBounds(193, 400, 170, 51);
 		frmViewFightDamage.getContentPane().add(btnFightDamage);
+		
+		JLabel lblYourEnemy = new JLabel("Your Enemy:");
+		lblYourEnemy.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblYourEnemy.setHorizontalAlignment(SwingConstants.CENTER);
+		lblYourEnemy.setBounds(50, 243, 450, 30);
+		frmViewFightDamage.getContentPane().add(lblYourEnemy);
+		
+		JLabel lblYourMonster = new JLabel("Your Monster:");
+		lblYourMonster.setHorizontalAlignment(SwingConstants.CENTER);
+		lblYourMonster.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblYourMonster.setBounds(50, 90, 450, 30);
+		frmViewFightDamage.getContentPane().add(lblYourMonster);
+		
+		JLabel lblFightOutcome = new JLabel("Fight Damage");
+		lblFightOutcome.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblFightOutcome.setHorizontalAlignment(SwingConstants.CENTER);
+		lblFightOutcome.setBounds(50, 30, 450, 30);
+		frmViewFightDamage.getContentPane().add(lblFightOutcome);
 	}
 
 }
