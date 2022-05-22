5e8cec2eee1e2bf893c3604fedc7fd1b53cad250
dannyjb164 <dannyjb164@gmail.com>
2022-05-22 23:39:50 +1200

    Updated sleep screen gui

--- monster-fighter/src/gui/GameSleepScreen.java
+++ monster-fighter/src/gui/GameSleepScreen.java
@@ -69,38 +69,38 @@ public class GameSleepScreen {
 	 */
 	private void initialize() {
 		frmGameSleep = new JFrame();
-		frmGameSleep.setBounds(100, 100, 450, 449);
+		frmGameSleep.setBounds(100, 100, 560, 575);
 		frmGameSleep.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmGameSleep.getContentPane().setLayout(null);
 		
-		JLabel lblGameSleepTitle = new JLabel("You go to Sleep for the Day");
+		JLabel lblGameSleepTitle = new JLabel("You Go To Sleep For The Day");
 		lblGameSleepTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		lblGameSleepTitle.setFont(new Font("Verdana", Font.BOLD, 17));
-		lblGameSleepTitle.setBounds(75, 11, 281, 29);
+		lblGameSleepTitle.setBounds(50, 30, 450, 29);
 		frmGameSleep.getContentPane().add(lblGameSleepTitle);
 		
 		JLabel lblGameSleepDay = new JLabel("Day " + gameEnviro.getGameDay() + " has Ended");
 		lblGameSleepDay.setHorizontalAlignment(SwingConstants.LEFT);
 		lblGameSleepDay.setFont(new Font("Verdana", Font.BOLD, 14));
-		lblGameSleepDay.setBounds(31, 51, 233, 29);
+		lblGameSleepDay.setBounds(40, 65, 233, 29);
 		frmGameSleep.getContentPane().add(lblGameSleepDay);
 		
 		JLabel lblGameSleepShop = new JLabel("- The Shop will have new stock tomorrow");
 		lblGameSleepShop.setHorizontalAlignment(SwingConstants.LEFT);
 		lblGameSleepShop.setFont(new Font("Verdana", Font.BOLD, 13));
-		lblGameSleepShop.setBounds(31, 80, 325, 29);
+		lblGameSleepShop.setBounds(40, 100, 325, 29);
 		frmGameSleep.getContentPane().add(lblGameSleepShop);
 		
 		JLabel lblGameSleepBattle = new JLabel("- There will be new Battles available tomorrow");
 		lblGameSleepBattle.setHorizontalAlignment(SwingConstants.LEFT);
 		lblGameSleepBattle.setFont(new Font("Verdana", Font.BOLD, 13));
-		lblGameSleepBattle.setBounds(31, 110, 362, 29);
+		lblGameSleepBattle.setBounds(40, 130, 362, 29);
 		frmGameSleep.getContentPane().add(lblGameSleepBattle);
 		
 		JLabel lblGameSleepMonster = new JLabel("- Your Monster(s) Heal and reset stats");
 		lblGameSleepMonster.setHorizontalAlignment(SwingConstants.LEFT);
 		lblGameSleepMonster.setFont(new Font("Verdana", Font.BOLD, 13));
-		lblGameSleepMonster.setBounds(31, 139, 325, 29);
+		lblGameSleepMonster.setBounds(40, 160, 325, 29);
 		frmGameSleep.getContentPane().add(lblGameSleepMonster);
 		
 		JButton btnGameSleep = new JButton("Continue");
@@ -120,32 +120,32 @@ public class GameSleepScreen {
 			}
 		});
 		btnGameSleep.setFont(new Font("Verdana", Font.BOLD, 15));
-		btnGameSleep.setBounds(126, 352, 173, 47);
+		btnGameSleep.setBounds(150, 400, 250, 47);
 		frmGameSleep.getContentPane().add(btnGameSleep);
 		
-		if (levelUp) {
+		//if (levelUp) {
 			JLabel lblGameSleepMonLvl = new JLabel("- A Monster Leveled up Overnight");
 			lblGameSleepMonLvl.setHorizontalAlignment(SwingConstants.LEFT);
 			lblGameSleepMonLvl.setFont(new Font("Verdana", Font.BOLD, 13));
-			lblGameSleepMonLvl.setBounds(31, 207, 325, 29);
+			lblGameSleepMonLvl.setBounds(40, 220, 325, 29);
 			frmGameSleep.getContentPane().add(lblGameSleepMonLvl);
-		}
+		//}
 		
-		if (monsterJoins) {
+		//if (monsterJoins) {
 			JLabel lblGameSleepMonJoin = new JLabel("- A Monster Joined your Party Overnight");
 			lblGameSleepMonJoin.setHorizontalAlignment(SwingConstants.LEFT);
 			lblGameSleepMonJoin.setFont(new Font("Verdana", Font.BOLD, 13));
-			lblGameSleepMonJoin.setBounds(31, 247, 325, 29);
+			lblGameSleepMonJoin.setBounds(40, 260, 325, 29);
 			frmGameSleep.getContentPane().add(lblGameSleepMonJoin);
-		}
+		//}
 		
-		if (monsterLeaves) {
+		//if (monsterLeaves) {
 			JLabel lblGameSleepMonLeave = new JLabel("- A Monster Left your Party Overnight");
 			lblGameSleepMonLeave.setHorizontalAlignment(SwingConstants.LEFT);
 			lblGameSleepMonLeave.setFont(new Font("Verdana", Font.BOLD, 13));
-			lblGameSleepMonLeave.setBounds(31, 287, 325, 29);
+			lblGameSleepMonLeave.setBounds(40, 300, 325, 29);
 			frmGameSleep.getContentPane().add(lblGameSleepMonLeave);
-		}
+		//}
 		
 		
 	}
