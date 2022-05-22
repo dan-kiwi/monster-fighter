caba679fd8b058ceed9c552740cf24ba7ce600bb
dannyjb164 <dannyjb164@gmail.com>
2022-05-22 15:59:44 +1200

    Fixed random item test

--- monster-fighter/src/test/RandomEventTest.java
+++ monster-fighter/src/test/RandomEventTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import mainenviro.GameEnviro;
@@ -20,6 +18,12 @@ class RandomEventTest {
 	private String[] masterMonsters = GameEnviro.getMasterMonsterList();
 	private RandomEvent rand = new RandomEvent(masterMonsters, userMonsters);
 	
+	/**
+	 * Helper method to check how many times a monster leaves after 1,000,000 trials
+	 *
+	 * @param monster the monster
+	 * @return the number of times a monster leaves
+	 */
 	int monsterLeaves(Monster monster) {
 		int tfCouner = 0;
 		for (int i = 0; i < 1000000; i++) {
@@ -28,12 +32,20 @@ class RandomEventTest {
 		return tfCouner;
 	}
 	
+	/**
+	 * A monster with health above zero should leave 1% of the time when the monsterLeaves method is called
+	 * Allows for some difference around 10,000 for expected varaiance
+	 */
 	@Test
 	void monsterLeavesHealthy() {
 		int tfCounter = monsterLeaves(new Dragon());
 		assertTrue(tfCounter > 9500 && tfCounter < 10500);
 	}
 	
+	/**
+	 * A monster with health zero should leave 2.5% of the time when the monsterLeaves method is called
+	 * Allows for some difference around 25,000 for expected varaiance
+	 */
 	@Test
 	void monsterLeavesUnhealthy() {
 		Monster monster = new Dragon();
@@ -42,6 +54,12 @@ class RandomEventTest {
 		assertTrue(tfCounter > 24500 && tfCounter < 25500);
 	}
 	
+	/**
+	 * Test checks if the levelUp method in randomEvent is working
+	 * Monster should level up based on how many battles they won that day
+	 * 10% for no wins then add 10 percentage points for every win until 100% at 9 wins
+	 * Checks for 10 and 11 wins to ensure no unexpected behaviour
+	 */
 	@Test
 	void monsterLevelUp() {
 		Monster monster = new Dragon();
